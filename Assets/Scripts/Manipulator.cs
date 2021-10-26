6e4caf0bbeaf1a654843e85bcecd0de71e36e4f9
Jazz <mh216347@gmail.com>
2021-10-26 00:53:13 -0600

    Project files

---
+++ Assets/Scripts/Manipulator.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Use transform.Translate and pass on user-defined values of x, y, and z if the user selects the Transform option.
+/// 
+/// Use transform.Rotate and pass on user-defined values of x, y, and z if the user selects the Rotation option.
+/// Repeat the same for the scale.
+/// 
+/// For reset, just restore the original values of X, Y, Z to the gameObject’s local.transform, rotation and scale
+/// → you have already stored the initial positions in the Start(), so just restore them back.
+/// 
+/// &&&
+///
+/// When you scale the object, you should scale it for only one frame (like I showed in the video).
+/// The GameObject should NOT constantly scale and keep expanding.
+///
+/// &&&
+///
+/// if I enable the boolean value of any one of these(translate,rotate,scale , and reset), then the boolean values for the rest of the three components should be disabled in the inspector.
+/// Which means at a given time I should be able to apply only one functionality(either translate/rotate/scale).
+///
+/// This requirement holds 10 points in this part of the assignment.
+/// </summary>
+public class Manipulator : MonoBehaviour
+{
+    
+    public bool transformObject = false;
+    public bool rotateObject = false;
+    public bool scaleObject = false;
+    public bool reset = false;
+    public float speed = 2.0f;
+   
+    
+    public float _xAxis;
+    public float _yAxis;
+    public float _zAxis;
+    
+    private Vector3 startPosition;
+  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+        // Uses user input for x,y and z axis to transform, rotate or scale objectß
+
+        if(transformObject == true)
+        {
+            rotateObject = false;
+            scaleObject = false;
+            reset = false;
+            transform.Translate(new Vector3(_xAxis, _yAxis, _zAxis)*Time.deltaTime);
+        }
+
+        else if (rotateObject == true)
+        {
+            transformObject = false;
+            scaleObject = false;
+            reset = false;
+            transform.Rotate(new Vector3(_xAxis, _yAxis, _zAxis) * Time.deltaTime);
+        }
+
+        else if (scaleObject == true)
+        {
+            transformObject = false;
+            rotateObject = false;
+            reset = false;
+            transform.localScale = (new Vector3(_xAxis, 1, _zAxis));
+            
+        }
+
+        else if(reset == true)
+        {
+            transformObject = false;
+            rotateObject = false;
+            scaleObject = false;
+            transform.position = startPosition;
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.rotation = Quaternion.identity;
+
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
+    public Text timerText;
+    private float startTime;
+    public GameObject Obstacle;
+    private Vector3 startPosition;
+    private bool finished = false;
+    public string seconds;
+    float endTimer;
+    public bool isTimerOn = true;
+
+    private void Start()
+
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        startTime = Time.time;
+        startPosition = transform.position;
+    }
+
+    public void Update()
+    {
+        float t = Time.time - startTime;
+        string minutes = ((int)t / 60).ToString();
+        seconds = (t % 60).ToString("f2");
+
+        if (isTimerOn)
+        {
+            timerText.text = minutes + ":" + seconds;
+        }
+        if (endTimer >= 30)
+        {
+           
+            isTimerOn = false;
+        }
+        
+
+        endTimer = (t % 60);
+       
+
+       
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Ups"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+            
+        }
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+
+            transform.position = startPosition;
+          
+
+        }
+        else if (other.gameObject.CompareTag("End Goal"))
+        {
+
+            Finnish();
+
+
+        }
+    }
+
+    public void Finnish()
+
+
+    {
+        if (count == 7 && isTimerOn == true)
+        {
+            winText.text = "You Win!";  //finished = true;
+        }
+        if(count != 7 && isTimerOn == true)
+        {
+            winText.text = "Level failed!";
+        }
+        else if (count == 7 && isTimerOn == false)
+        {
+            winText.text = "Level failed!"; // finished = false;
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+       
+    }
+}
